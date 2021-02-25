649d4d8eb9a68ad9087dfcc3de9db434280a65d9
aleksandr <valakh2607@yandex.ru>
2021-02-25 08:45:05 +0300

    добавление новых файлов

---
+++ app/app.py
@@ -0,0 +1,7 @@
+from flask import Flask
+from config import Configuration
+
+app = Flask(__name__)
+
+app.config.from_object(Configuration) # передавать в словарь app.config настройки приложения через 
+                                        #метод from_object
